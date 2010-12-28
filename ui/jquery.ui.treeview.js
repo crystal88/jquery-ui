310e2279eba69d8134c0dbc4b2f6db768ed3cd9a
crystal@crystal-desktop <ebence88@gmail.com>
2010-12-28 18:26:26 +0100

    selection variable introduced

--- ui/jquery.ui.treeview.js
+++ ui/jquery.ui.treeview.js
@@ -1,19 +1,21 @@
-$.widget("ui.checkboxtree", {
+$.widget("ui.treeview", {
 
-	'root_nodes' : {},
+	'rootNodes' : {},
+	
+	'currentSelection' : null,
 	
 	'_init' : function() {
 		domelem = this.element[0];
 		domelem.setAttribute('class', 'ui-widget ui-widget-content ui-corner-all');
 		$('h1', domelem).attr('class', 'ui-widget-header ui-corner-all ui-helper-reset-ui-helper-clearfix');
 		$('h1', domelem).attr('style', 'font-size: 100%; margin-top: 2px');
-		checkboxtree = this;
-		this.init(domelem, this.root_nodes);
+		treeview = this;
+		this.init(domelem, this.rootNodes);
 		$('ul li ul', domelem).hide();
 	},
 	
 	'init' : function(domelem, container) {
-		var checkboxtree = this;
+		var treeview = this;
 		container['children'] = {};
 		
 		$('ul > li', domelem).each((function(nodecont) {
@@ -25,29 +27,28 @@ $.widget("ui.checkboxtree", {
 						nodecont[$(this).attr('name')] = $(this).val();
 					});
 					if ($('ul', this).length) {
-						$('> input', this).before('<span class="ui-icon ui-icon-carat-1-e" style="display:inline-block; margin-left: -16px; cursor:pointer"></span>');
+						$(this).prepend('<span class="ui-icon ui-icon-carat-1-e" style="display:inline-block; margin-left: -16px; cursor:pointer"></span>');
 					}
-					$('span', this).click(checkboxtree.createClickListener(parent));
-					//$('a', this).each(checkboxtree.add_toggle_listener(parent));
+					$('span', this).click(treeview.createClickListener(parent, treeview));
 					$('a', this).each((function(parent) {
 						return function() {
 							if (this.parentNode == parent) {
-								$(this).attr('class','ui-priority-primary');
 								$(this).attr('style', 'text-decoration:none; bottom: 3px; position: relative; cursor:pointer');								
-								$(this).click(checkboxtree.createClickListener(parent));
+								$(this).click(treeview.createClickListener(parent, treeview));
 							}
 						}
 					})(parent)
 					
 					);
-					checkboxtree.init(this, nodecont);
+					treeview.init(this, nodecont);
 				}
 		}})(container['children']));
 		$(domelem).attr('style', 'list-style-type:none; list-style-position:inside; margin-left: -10px');
 	},
 	
-	'createClickListener' : function(parent){
+	'createClickListener' : function(parent, treeview){
 					return function() {
+						treeview.changeSelection($('> a', parent));
 						hiddens = $('> ul:hidden', parent);
 						if (hiddens.length == 1) {
 							hiddens.show();
@@ -60,7 +61,20 @@ $.widget("ui.checkboxtree", {
 				},
 				
 	'result' : function() {
-		return this.root_nodes;
+		return this.rootNodes;
+	},
+	
+	'changeSelection' : function(newSel) {
+		var selectionClass = 'ui-state-focus';
+		if (treeview.currentSelection != null) {
+			treeview.currentSelection.removeClass(selectionClass);
+		}
+		treeview.currentSelection = newSel;
+		treeview.currentSelection.addClass(selectionClass);
+	},
+	
+	'sg' : function(a) {
+		console.log('hello: '+a);
 	}
 });
 
